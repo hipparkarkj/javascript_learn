ad8754c136a6015554256a1f46f9ddd5abd8e001
hipparkarkj <hipparkarkj@gmail.com>
2024-11-14 10:26:45 +0000

    updated

--- 01_javascript_basics/01_basics/03_conversionOperation.js
+++ 01_javascript_basics/01_basics/03_conversionOperation.js
@@ -5,11 +5,60 @@ let score = "kailas"
 
 let valueInNumber =Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
  
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
 let isLoggedIn = "kailas"
+
+let boolleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolleanIsLoggedIn);
+
+// 1=> true; 0 => false
+// "" => false
+// "kailas" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//**************************opertaions *****************
+
+let value = 3
+let negValue = -value
+ //console.log(negValue)
+
+ // console.log(2+2);
+ // console.log(2-2);
+ // console.log(2*2);
+ // console.log(2**3);
+ // console.log(2/3);
+ // console.log(2%3);
+
+ let str1 = "hello"
+ let str2 = " kailas"
+
+ let str3 = str1 + str2
+ // console.log(str3)
+
+
+// console.log("1" + 2);
+
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(( 3 + 4 ) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+
+console.log(gameCounter)
