2b02191f10d648a3da575f489f4a4d2422545b33
hipparkarkj <hipparkarkj@gmail.com>
2024-11-14 13:51:17 +0000

    update

---
+++ 01_javascript_basics/01_basics/07_DatesinJs.js
@@ -0,0 +1,13 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+//console.log(myDate);
+console.log(myDate.toDateString());
+
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024, 0, 23)
+let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+console.log(myCreatedDate.toLocaleDateString());
