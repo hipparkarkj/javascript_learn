29af44ce99a9e4fd22e32b0a5ef62f39dfa7e6aa
hipparkarkj <hipparkarkj@gmail.com>
2024-11-16 11:35:21 +0000

    iteration_all updated

--- 01_javascript_basics/05_iterations/one.js
+++ 01_javascript_basics/05_iterations/one.js
@@ -6,11 +6,30 @@ for(let i=0; i<=10; i++){
     //console.log(element);
 }
 for(let i=1; i<=10; i++){
-    console.log(`outer loop: ${i}`);
+    // console.log(`outer loop: ${i}`);
     for(let j=1; j<=10; j++){
-        console.log(`innner loop value ${j} and inner loop value ${i}`)
+        // console.log(`innner loop value ${j} and inner loop value ${i}`)
+        // console.log(i + '*' + j + ' = ' + i*j );
     }
 
 }
-let myArry
+
+let myArray = ["flash", "batman", "superman"]
+console.log(myArray.length);
+
+for (let index =0; index <= 20; index++){
+    if (index == 5){
+        console.log(`detected 5`);
+        break;
+    }
+    console.log(`Value of i is ${index}`);
+}
     
+for (let index =1; index <=20; index++){
+    if (index == 5) {
+        console.log('detected 5');
+        continue
+    }
+    console.log(`Value of i is ${index}`);
+
+}
