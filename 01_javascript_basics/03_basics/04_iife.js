72065ee951a9ef0c344674eea1fe46d5b0f6228c
hipparkarkj <hipparkarkj@gmail.com>
2024-11-16 09:10:21 +0000

    IIFE_update

---
+++ 01_javascript_basics/03_basics/04_iife.js
@@ -0,0 +1,13 @@
+// Immediatly Invoked Functions Expressions (IIFE)
+
+
+(function chai(){
+
+    // named IIFE
+    console.log(`DB CONNECTED`);
+
+})();
+
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`)
+})("hites")
